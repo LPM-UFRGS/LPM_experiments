3d4ce6c285cf185c645510076ec032f3e0409b9e
gogo40 <pericles.raskolnikoff@gmail.com>
2014-04-09 14:14:50 -0300

    Initial code to variogram calculation using FFT (1-D version)

---
+++ 1D/run_benchmark.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+g++ -O2 -o variogram_simple variogram_simple.cpp -lm 
+g++ -O2 -o variogram_fft variogram_fft.cpp -lm -lfftw3 -lfftw3l
+
+
+echo "Testing simple N = 10 ..."
+time ./variogram_simple < t10.in > t10.s.out
+
+
+echo "Testing simple N = 1000 ..."
+time ./variogram_simple < t1000.in > t1000.s.out
+
+echo "Testing simple N = 10000 ..."
+time ./variogram_simple < t10000.in > t10000.s.out
+
+#echo "Testing simple N = 100000 ..."
+#time ./variogram_simple < t100000.in > t100000.s.out
+
+
+
+
+echo "Testing FFT N = 10 ..."
+time ./variogram_fft < t10.in > t10.fft.out
+diff t10.s.out t10.fft.out
+
+
+echo "Testing FFT N = 1000 ..."
+time ./variogram_fft < t1000.in > t1000.fft.out
+diff t1000.s.out t1000.fft.out
+
+echo "Testing FFT N = 10000 ..."
+time ./variogram_fft < t10000.in > t10000.fft.out
+diff t10000.s.out t10000.fft.out
+
+echo "Testing FFT N = 100000 ..."
+time ./variogram_fft < t100000.in > t100000.fft.out
+
+echo "Testing FFT N = 1000000 ..."
+time ./variogram_fft < t1000000.in > t1000000.fft.out
+
+#echo "Testing FFT N = 10000000 ..."
+#time ./variogram_fft < t10000000.in > t10000000.fft.out
+
+
+
+
---
+++ 1D/variogram_simple_1d.cpp
@@ -0,0 +1,47 @@
+/*
+Este programa calcula o variograma numa direção usando a definição diretamente.
+
+
+Autor: Péricles Lopes Machado
+
+*/
+
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	
+	double* z = new double[N];
+	double* Y = new double[N];
+
+	for (int i = 0; i < N; ++i) {
+		scanf("%lf", &z[i]);
+	}
+
+
+	Y[0] = 0;
+	for (int h = 0; h < N; ++h) {
+		double acc = 0;
+		double accprod = 0;
+		for (int i = h; i < N; ++i) {
+			double dz = z[i] - z[i - h];
+			// z[i]² + z[i - h]² - 2 z[i]z[i-h]
+			acc += 	(dz * dz) / (2 * (N - h));
+			//accprod += z[i] * z[i - h];
+		}
+
+		printf("%.3lf\n", acc);
+		//printf("%.3lf\n", accprod);
+
+	}
+
+	delete z;	
+	return 0;
+}
+
+
