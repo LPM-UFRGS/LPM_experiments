f34aa81f8c26d5e74e93d594c2317e703f8c3d5d
gogo40 <pericles.raskolnikoff@gmail.com>
2014-06-05 00:40:09 -0300

    small changes

--- 2D/gen_data.cpp
+++ 2D/gen_data.cpp
@@ -13,11 +13,24 @@ int main()
 	scanf("%d%d", &N, &M);
 
 	printf("%d %d\n", N, M);
+
+	FILE* fout = fopen("data_2d.dat", "w+");
+	fprintf(fout, "Varmap fft 2d\n");
+	fprintf(fout, "3\n");
+	fprintf(fout, "X\n");
+	fprintf(fout, "Y\n");
+	fprintf(fout, "data\n");
+
 	for (int i = 0; i < N; ++i) { 
-		for (int j = 0; j < M; ++j)
-			printf("%d ", rand() % 200);
+		for (int j = 0; j < M; ++j) {
+			double v = rand() % 200;
+			printf("%lf ", v);
+			fprintf(fout, "%d %d %lf\n", i, j, v);
+		
+		}
 		printf("\n");
 	}
+	fclose(fout);
 
 	return 0;
 }
--- 3D/gen_data.cpp
+++ 3D/gen_data.cpp
@@ -13,14 +13,27 @@ int main()
 	scanf("%d%d%d", &N, &M, &K);
 
 	printf("%d %d %d\n", N, M, K);
+
+	FILE* fout = fopen("data_3d.dat", "w+");
+
+	fprintf(fout, "Varmap fft 3d\n");
+	fprintf(fout, "4\n");
+	fprintf(fout, "X\n");
+	fprintf(fout, "Y\n");
+	fprintf(fout, "Z\n");
+	fprintf(fout, "data\n");
+
 	for (int i = 0; i < N; ++i) { 
 		for (int j = 0; j < M; ++j) {
 			for (int k = 0; k < K; ++k) {
-				printf("%d ", rand() % 200);
+				double v = rand() % 200;
+				printf("%lf ", v);
+				fprintf(fout, "%d %d %d %lf\n", i, j, k, v);
 			}
 			printf("\n");
 		}
 	}
+	fclose(fout);
 
 	return 0;
 }
